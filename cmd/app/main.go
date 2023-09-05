4739a0824fbb8d114bc255dd97270c78e5ce31f6
Maksim Shebordaev <mshebordaev@digital-spirit.ru>
2023-09-05 22:34:49 +0300

    Fixed main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	cfg "github.com/maksewsha/wardrobe-back/cfg"
 	application "github.com/maksewsha/wardrobe-back/internal/app"
 )
 
 func main() {
-	cfg := cfg.New()
+	cfg, err := cfg.New()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error to create config: %v\n", err)
+	}
 	application.Start(cfg)
 }
